86cacf47f5f704cf4397485679b9a21b95323eac
Sazzad Hossen <mshossen75@gmail.com>
2023-03-07 21:56:21 +0600

    working on salary calculation

--- employee/urls.py
+++ employee/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 # import views
 from employee.views import employee_main
 from employee.views import manage_leave
+from employee.views import manage_salary
 
 app_name='employee'
 
@@ -17,4 +18,11 @@ urlpatterns += [
     path('update-leave/<int:pk>/', manage_leave.LeaveApplicationUpdateView.as_view(), name="update_leave" ),
     path('leave-details/<int:pk>/', manage_leave.LeaveApplicationDetailsView.as_view(), name="leave_details" ),
 ]
+
+# Manage Salary
+urlpatterns += [
+    path('monthly-salary-list/', manage_salary.MonthlySalaryListView.as_view(), name='monthly_salary_list'),
+    path('employee-salary-details/<int:pk>/', manage_salary.EmployeeMonthlySalaryDetailsView.as_view(), name='employee_salary_details'),
+]
+
 
---
+++ employee/views/manage_salary.py
@@ -0,0 +1,36 @@
+
+# Generic Classes
+from django.views.generic import ListView
+from django.views.generic import DetailView
+
+
+# Permission Classes
+from django.contrib.auth.mixins import LoginRequiredMixin
+from employee.permission import EmployeePassesTestMixin
+
+# Models
+from employee.models import MonthlySalary
+
+
+class MonthlySalaryListView(LoginRequiredMixin, EmployeePassesTestMixin, ListView):
+    model = MonthlySalary
+    template_name = 'employee/employee_salary_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Monthly Salary List" 
+        context["salarys"] = MonthlySalary.objects.filter(is_active=True,salary_employee=self.request.user.employee_info ).order_by('-id')
+        return context
+
+class EmployeeMonthlySalaryDetailsView(LoginRequiredMixin, EmployeePassesTestMixin, DetailView):
+    model = MonthlySalary
+    context_object_name = 'salary'
+    template_name = 'employee/salary_details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Monthly Salary Details" 
+        return context
+    
+
+
